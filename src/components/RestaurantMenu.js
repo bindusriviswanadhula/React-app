00eb694d4071657627cb154c6ebeb7588ed4ad4c
bindusriviswanadhula <bindusriviswanadhula45@gmail.com>
2024-05-23 10:21:08 +0530

    updates

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -16,13 +16,13 @@ const [showIndex, setShowIndex] = useState(null);
     if(resInfo === null) return <Shimmer/>;
 
 
-    const { name, cuisines, costForTwoMessage } = resInfo?.cards[0]?.card?.card?.info;
+    const { name, cuisines, costForTwoMessage } = resInfo?.cards[2]?.card?.card?.info;
 
-    const{itemCards} = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
+    const{itemCards} = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
 
-    console.log(resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards);
+    console.log(resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards);
 
-    const categories = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+    const categories = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
         (c) =>
         c.card?.card?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
     );
@@ -42,7 +42,7 @@ const [showIndex, setShowIndex] = useState(null);
              key={category.card?.card.title}
              data={category.card?.card}
              showItems={index === showIndex ? true : false}
-             setShowIndex={() => setShowIndex(index)}
+             setShowIndex={() => setShowIndex(index==showIndex?null:index)}
              />
              ))}
  
