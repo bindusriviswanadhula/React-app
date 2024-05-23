8ae81aa3100d70336c6668f94a07328966914854
bindusriviswanadhula <bindusriviswanadhula45@gmail.com>
2024-05-23 22:32:57 +0530

    replaced react server urls.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,12 +12,13 @@ export const RESMENU_URL1 =
 export const RESMENU_URL =
 
 // "https://lucky-tick-gabardine.cyclic.app/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=12.9709949&lng=77.6886853&restaurantId=";
-
-"https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9709949&lng=77.6886853&restaurantId=";
+"https://react-app-server-vha7.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=12.9709949&lng=77.6886853&restaurantId=";
+// "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9709949&lng=77.6886853&restaurantId=";
 
 
 
  export const RESTAURANTS_URL =
 
 //  "https://lucky-tick-gabardine.cyclic.app/api/restaurants?lat=12.9709949&lng=77.6886853&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
- "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9709949&lng=77.6886853&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+ "https://react-app-server-vha7.onrender.com/api/restaurants?lat=12.9709949&lng=77.6886853&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+//  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9709949&lng=77.6886853&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
